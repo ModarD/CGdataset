4cc1feb220d615b2921c19fa0ff16d5cb81547bd
ModarD <skullyubel@gmail.com>
2018-11-23 15:59:13 +0100

    params

--- train_shape_predictor.py
+++ train_shape_predictor.py
@@ -70,10 +70,10 @@ options.be_verbose = True
 # set the parameters
 #options.tree_depth = 4
 options.nu = 0.05#0.025#0.05#0.01#0.005#
-options.cascade_depth = 20#15 
+options.cascade_depth = 15 
 options.feature_pool_size = 1000#800#
-options.num_test_splits = 150#100#20#20#
-options.oversampling_amount = 20#40#80#
+options.num_test_splits = 20#150#20#100#
+options.oversampling_amount = 80#20#80#
 options.num_threads = 16
 
 options.lambda_param = 0.01#0.005#
